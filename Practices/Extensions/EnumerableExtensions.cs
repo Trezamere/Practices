7581c9d9880abb0a8fe51929d87bd75d3d673715
Trezamere <trezamere@outlook.com>
2014-07-22 17:00:21 -0400

    Adding some comments to ForEach

--- Practices/Extensions/EnumerableExtensions.cs
+++ Practices/Extensions/EnumerableExtensions.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 
 namespace Practices.Extensions
 {
+    /// <summary>
+    /// Contains the IEnumerable extension methods.
+    /// </summary>
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Performs the specified action on each element of the <see cref="IEnumerable{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the objects in <paramref name="source"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> that contains the source collection.</param>
+        /// <param name="action">The <see cref="Action{T}"/> delegate to perform on each element of the <see cref="IEnumerable{T}"/>.</param>
         public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
             foreach (var value in source)
