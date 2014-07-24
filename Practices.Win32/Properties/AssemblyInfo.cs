516a5766f35e3734948df8a44f0ebbc0122f7f62
Trezamere <trezamere@outlook.com>
2014-07-24 17:22:44 -0400

    Including the DwaneNeed win32 library http://microsoftdwayneneed.codeplex.com/

---
+++ Practices.Win32/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Practices.Win32")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b767ea6-9571-4e5f-a1fd-114e12657215")]
---
+++ Practices.Win32/User32/GCW.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practices.Win32.User32
+{
+    /// <summary>
+    ///     Class field offsets for GetClassWord
+    /// </summary>
+    public enum GCW : int
+    {
+        ATOM = -32
+    }
+}
