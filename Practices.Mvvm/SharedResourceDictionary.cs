698ed25d6b92ac12dc74e61b8dd536b08c925e4d
Trezamere <trezamere@outlook.com>
2014-07-24 17:33:28 -0400

    Updating SharedResourceDictionary

--- Practices.Mvvm/SharedResourceDictionary.cs
+++ Practices.Mvvm/SharedResourceDictionary.cs
@@ -1,80 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Markup;
 
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Practices.Mvvm")]
 namespace Practices.Mvvm
 {
-	/// <summary>
-	/// Taken from http://wpftutorial.net/MergedDictionaryPerformance.html
+	/// <summary> 
 	/// The shared resource dictionary is a specialized resource dictionary
 	/// that loads it content only once. If a second instance with the same source
 	/// is created, it only merges the resources from the cache.
 	/// </summary>
+	/// <remarks>
+    /// Taken from http://wpftutorial.net/MergedDictionaryPerformance.html
+	/// </remarks>
 	public sealed class SharedResourceDictionary : ResourceDictionary
 	{
-		private static readonly Dictionary<string, WeakReference> SharedDictionaries = new Dictionary<string, WeakReference>();
+        /// <summary>
+        /// Initializes static members of the <see cref="SharedResourceDictionary"/> class.
+        /// </summary>
+        static SharedResourceDictionary()
+        {
+            IsInDesignerMode = (bool)DesignerProperties.IsInDesignModeProperty.GetMetadata(typeof(DependencyObject)).DefaultValue;
+        }
 
-		private static ResourceDictionary GetResourceDictionary(string dictionaryName)
-		{
-			ResourceDictionary result = null;
-			if (SharedDictionaries.ContainsKey(dictionaryName))
-			{
-				result = (ResourceDictionary)SharedDictionaries[dictionaryName].Target;
-			}
-			if (result == null)
-			{
-				string assemblyName = System.IO.Path.GetFileNameWithoutExtension(
-					Assembly.GetExecutingAssembly().ManifestModule.Name);
-				result = Application.LoadComponent(new Uri(assemblyName
-					+ ";component/Resources/" + dictionaryName + ".xaml",
-					UriKind.Relative)) as ResourceDictionary;
-				SharedDictionaries[dictionaryName] = new WeakReference(result);
-			}
-			return result;
-		}
+        /// <summary>
+        /// Internal cache of loaded dictionaries.
+        /// </summary>
+        private static readonly Dictionary<Uri, WeakReference> SharedDictionaries = new Dictionary<Uri, WeakReference>();
 
-		#region MergedDictionaries DependencyProperty
+        /// <summary>
+        /// A value indicating whether the application is in designer mode.
+        /// </summary>
+        private static readonly bool IsInDesignerMode;
 
-		public static readonly DependencyProperty MergedDictionariesProperty =
-			DependencyProperty.RegisterAttached("MergedDictionaries", typeof (string), typeof (SharedResourceDictionary),
-				new FrameworkPropertyMetadata(null, OnMergedDictionariesChanged));
+        /// <summary>
+        /// Backing field for <see cref="Source"/>.
+        /// </summary>
+        private Uri _source;
+        /// <summary>
+        /// Gets or sets the uniform resource identifier (URI) to load resources from.
+        /// </summary>
+        public new Uri Source
+        {
+            get { return _source; }
+            set
+            {
+                _source = value;
 
-		public static string GetMergedDictionaries(DependencyObject d)
-		{
-			return (string) d.GetValue(MergedDictionariesProperty);
-		}
+                // Always load the dictionary by default in designer mode.
+                if (!SharedDictionaries.ContainsKey(value) || !SharedDictionaries[value].IsAlive || IsInDesignerMode)
+                {
+                    // If the dictionary is not yet loaded, load it by setting the source of the base class
+                    base.Source = value;
 
-		public static void SetMergedDictionaries(DependencyObject d, string value)
-		{
-			d.SetValue(MergedDictionariesProperty, value);
-		}
-
-		private static void OnMergedDictionariesChanged(DependencyObject d,
-			DependencyPropertyChangedEventArgs e)
-		{
-			if (!string.IsNullOrEmpty(e.NewValue as string))
-			{
-				foreach (string dictionaryName in (e.NewValue as string).Split(';'))
-				{
-					ResourceDictionary dictionary = GetResourceDictionary(dictionaryName);
-					if (dictionary != null)
-					{
-						if (d is FrameworkElement)
-						{
-							(d as FrameworkElement).Resources.MergedDictionaries.Add(dictionary);
-						}
-						else if (d is FrameworkContentElement)
-						{
-							(d as FrameworkContentElement).Resources.MergedDictionaries.Add(dictionary);
-						}
-					}
-				}
-			}
-		}
-
-		#endregion
+                    // add it to the cache if we're not in designer mode
+                    if (!IsInDesignerMode)
+                    {
+                        // If the old Dictionary went out of scope and we are using the same URI, this will remove the old WeakReference.
+                        SharedDictionaries[value] = new WeakReference(this);
+                    }
+                }
+                else
+                {
+                    // If the dictionary is already loaded, get it from the cache
+                    MergedDictionaries.Add((ResourceDictionary) SharedDictionaries[value].Target);
+                }
+            }
+        }
 	}
 }
